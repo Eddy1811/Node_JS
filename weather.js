bdd057730b9b995d365b89430984724d0a43d506
Eddy <eddy.vernet@le-campus-numerique.fr>
2024-04-10 09:08:16 +0200

    first commit -ex 3

---
+++ weather.js
@@ -0,0 +1,7 @@
+const weather = require('weather-js')
+
+weather.find({search: 'San Francisco, CA', degreeType: 'C'}, function(err,result) {
+    if(err) console.log(err);
+
+    console.log(JSON.stringify(result, null, 2));
+});
